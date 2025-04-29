51f42e95ae6dca44d4e571d6a17c88d7f50b01ae
Deepthikka Kambarayar <deepthikka.kambarayar@gomoder.com>
2025-04-29 12:08:17 +0530

    Fixed PDF filename display issue

--- pages/api/sharepoint/fileurl/[[...params]].ts
+++ pages/api/sharepoint/fileurl/[[...params]].ts
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import { fetchFileURL } from "../../../../lib/sharepoint"
-import { fileName } from "../../../../components/chatbot/Api";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "GET") {
